a201758cf12b363d18941bce3f32f02e841b2c46
jenniferchristine <jeja2306@student.miun.se>
2024-06-08 14:57:44 +0200

    added console logs to see where im going wrong

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,12 @@ async function logIn() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+    console.log("Användarnamn:", username);
+    console.log("Lösenord:", password);
+
     if (!username || !password) {
         alert("Fyll i både lösenord och användarnamn");
+        return;
     }
 
     // skicka förfrågan till servern
@@ -18,14 +22,21 @@ async function logIn() {
             body: JSON.stringify({ username, password })
         });
 
+        console.log("Svarsdata:", data);
+
         if (response.ok) {
             document.getElementById("confirmation").innerText = data.response.message;
             localStorage.setItem("token", data.response.token); // lagra token
-            console.log(token);
+
+            console.log("Token:", data.response.token);
+
         } else {
             document.getElementById("confirmation").innerText = data.error; // felmeddelande för test
+
+            console.log("Error", data.error);
         }
     } catch (error) {
+        console.error("Servererror:", error);
         document.getElementById("confirmation").innerText = "Serverfel";
     }
 }
