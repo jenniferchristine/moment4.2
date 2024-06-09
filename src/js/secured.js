d142067bd84e81839b76a2a90ae0329cc6d51625
jenniferchristine <jeja2306@student.miun.se>
2024-06-09 19:53:34 +0200

    added dom load event listener

--- src/js/secured.js
+++ src/js/secured.js
@@ -1,5 +1,19 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", function() {
+    const logOutBtn = document.getElementById("logoutBtn");
+
+    logOutBtn.addEventListener("click", async function (e) {
+        e.preventDefault();
+        logOut();
+    });
+});
+
+function logOut() {
+    localStorage.removeItem("token"); // ta bort token från localstorage
+    window.location.href = "logout.html";
+}
+
 const token = localStorage.getItem("token");
 
 if (!token) {
@@ -13,26 +27,21 @@ if (!token) {
             'Authorization': `Bearer ${token}`
         }
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Något gick fel vid åtkomst av skyddad resurs");
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.message === "Protected route...") {
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Något gick fel vid åtkomst av skyddad resurs");
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.message === "Protected route...") {
 
-        } else {
-            window.location.href = 'index.html'; // omdirigera om tokenen är ogiltig
-        }
-    })
-    .catch(error => {
-        console.error("Fel vid åtkomst av skyddad resurs:", error);
-        window.location.href = 'index.html';
-    });
-}
-
-function logOut() {
-    localStorage.removeItem("token"); // ta bort token från localstorage
-    window.location.href = "logout.html";
+            } else {
+                window.location.href = 'index.html'; // omdirigera om tokenen är ogiltig
+            }
+        })
+        .catch(error => {
+            console.error("Fel vid åtkomst av skyddad resurs:", error);
+            window.location.href = 'index.html';
+        });
 }
